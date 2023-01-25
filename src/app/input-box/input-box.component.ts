066d97e3968dfb4caf414eec72abd55017c8b84c
sriram-prasads <ssriramprasad2011@gmail.com>
2023-01-25 12:13:53 +0530

    added input box

---
+++ src/app/input-box/input-box.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'input-box',
+  templateUrl: './input-box.component.html',
+  styleUrls: ['./input-box.component.css']
+})
+export class InputBoxComponent {
+
+  
+}
