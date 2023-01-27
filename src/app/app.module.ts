bf6ecbf2d54606150c908d9e4a918a3a9b7650d3
sriram-prasads <ssriramprasad2011@gmail.com>
2023-01-27 09:39:08 +0530

    updated app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';   
+import { MatDividerModule } from '@angular/material/divider';
+
+import { FormsModule } from '@angular/forms';
+
+import {RouterModule} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputBoxComponent } from './input-box/input-box.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsComponent } from './posts/posts.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBoxComponent,
+    TabsComponent,
+    PostsComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    FormsModule,
+    RouterModule.forRoot([
+        { path:'', component: PostsComponent},
+        { path:'posts', component: PostsComponent},
+        
+
+
+    ]),
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input-box/input-box.component.ts
+++ src/app/input-box/input-box.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 
-
-
-
 @Component({
   selector: 'input-box',
   templateUrl: './input-box.component.html',
-  styleUrls: ['./input-box.component.css']
+  styleUrls: ['./input-box.component.css'],
 })
-export class InputBoxComponent {
-
-  
-}
+export class InputBoxComponent {}
